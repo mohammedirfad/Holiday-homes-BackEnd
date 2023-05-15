e90c784501322fd8435ae9aa463c25dbd6c90929
Mohammed irfad <muhammedirfad43@gmail.com>
2023-05-15 20:08:24 +0530

    s3 added succesfully

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import Message from './routes/user/MessageRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// app.use(express.json())
+
 app.use(morgan("tiny"));
 // app.use(express.urlencoded({ limit: "300kb", extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
