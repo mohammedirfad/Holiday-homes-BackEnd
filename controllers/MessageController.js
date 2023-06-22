80b08df9dfc701ccbac0d8e2f022818cc27d9147
Mohammed irfad <muhammedirfad43@gmail.com>
2023-06-22 21:38:40 +0530

    message completed

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -2,7 +2,7 @@ import MessageModel from '../models/MessageSchema.js';
 
 
 export const addMessage = async(req,res)=>{
-    console.log("hrer for sending", req.body)
+
     
     const {chatId , senderId ,text} = req.body.datas;
     console.log(chatId,senderId)
@@ -29,13 +29,12 @@ export const addMessage = async(req,res)=>{
 
 export const getMessages = async (req, res) => {
 
-    console.log("here where messaging!!!!")
 
     const {chatId}=req.params
 
     try{
         const result = await MessageModel.find({ChatId:chatId});
-        console.log("@messaging" ,result )
+        
         res.status(200).json(result)
 
     }
