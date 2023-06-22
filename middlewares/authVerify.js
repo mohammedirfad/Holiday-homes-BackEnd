bbd2f32c0412ff48380566d3b0337ea900d5f16c
Mohammed irfad <muhammedirfad43@gmail.com>
2023-06-22 21:36:23 +0530

    auth completed

--- middlewares/authVerify.js
+++ middlewares/authVerify.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 
 export const generateToken = (userId) => {
-  console.log(userId, 44111111111)
+ 
   const token = jwt.sign({ id: userId, role: userId.role }, process.env.JWT_SECRET_KEY, { expiresIn: "1hr" });
   return token;
 };
@@ -11,24 +11,24 @@ export const generateToken = (userId) => {
 
 export const verifyToken = async (req, res, next) => {
   try {
-    console.log("rehyeg iu5yovlue9ni6")
+
     let token = req.header("Authorization");
 
     if (!token) {
-      console.log("here is not token");
+      
       return res.status(403).send("Access Denied");
 
     }
 
     if (token.startsWith("Bearer")) {
       token = token.slice(7, token.length).trimLeft();
-      console.log("here is token");
+     
     } else {
       return res.status(403).send("Access Denied");
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log(verified, " ve");
+
 
     if (verified.id.role === "client-Login") {
     
